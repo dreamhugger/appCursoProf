d37dacc984aa572ad725c6d39699f0775dbd8776
dreamhugger <nostalgicdreamcast@gmail.com>
2023-10-23 22:35:29 -0300

    Aula 23/10

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use App\Http\Controllers\AulaController;
 Route::get('/',[CategoriaController::class,'index'])->name('index');
 
 //rotas para visualizar e cadastrar a categoria
-Route::get('/cadcategoria',[CategoriaController::class,'mostrarFormCat'])->name("form-cadastro-categoria");
-Route::post('/cadcategoria',[CategoriaController::class,'cadastroCat'])->name("cadastro-categoria");
+Route::get('/cadcategoria',[CategoriaController::class,'mostrarFormCategoria'])->name("form-cadastro-categoria");
+Route::post('/cadcategoria',[CategoriaController::class,'cadastroCategoria'])->name("cadastro-categoria");
 
 //rotas para manipular categoria
 Route::get('/manipulacategoria',[CategoriaController::class,'mostrarManipulaCategoria'])->name("manipula-categoria");
@@ -24,7 +24,10 @@ Route::get('/alterar-categoria/{registrosCategoria}',[CategoriaController::class
 Route::get('/cadcurso',[CursoController::class,'mostrarFormCurso'])->name("form-cadastro-curso");
 Route::post('/cadcurso',[CursoController::class,'cadastroCurso'])->name("cadastro-curso");
 
+Route::get('/manipulacurso',[CursoController::class,'mostrarManipulaCurso'])->name("manipula-curso");
+
 //rotas para visualizar e cadastrar a aula
 Route::get('/cadaula',[AulaController::class,'mostrarFormAula'])->name("form-cadastro-aula");
 Route::post('/cadaula',[AulaController::class,'cadastroAula'])->name("cadastro-aula");
 
+Route::get('/manipulaaula',[AulaController::class,'mostrarManipulaAula'])->name("manipula-aula");
